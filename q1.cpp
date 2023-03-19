1c796b34b1e91f4dec514d90e4cbb43e6535d429
Atifzaheer <128285262+rajAtif@users.noreply.github.com>
2023-03-19 16:05:01 +0500

    lab task 3  question 1

---
+++ q1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+//lab task 3 
+using namespace std ;
+int main()
+{
+	cout<<"#################"<<endl;
+	cout<<"$$$$$$$************$$$$"<<endl;
+	cout<<"********//////welcome to home******* "<<endl;
+	cout<<"********************"<<endl;
+	cout<<"#####################"<<endl;
+	return 0;
+	//coded bY Atif zaheer ;
+	
+	
+	
+	
+	
+	
+	
+	
+}
